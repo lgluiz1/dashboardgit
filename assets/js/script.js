42803d452fc9f78b053cc8761de43ee2c5e823f0
lgluiz1 <legalhints@gmail.com>
2023-03-20 02:03:50 -0300

    dashboardgit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function sidebarMenu(){
+    if (sidebar.style.display == 'block'){
+        sidebar.style.display = 'none'
+}
+    else{
+        sidebar.style.display = 'block'
+    }
+}
+
+function nameMenu(){
+    if ( name_menu_out.style.scale == '0'){
+         name_menu_out.style.scale = '1'
+    }else{
+        name_menu_out.style.scale = '0'
+    }
+}
